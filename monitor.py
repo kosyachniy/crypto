996d71372789d54bde66c1c62b6236de727e51b4
Poloz Alexey <polozhev@mail.ru>
2017-09-25 14:58:15 +0300

    Worked message

--- monitor.py
+++ monitor.py
@@ -1,8 +1,10 @@
-from time import sleep
+from func import *
 
-def monitor(x):
-	while True:
-		#x['trade'].append([1, 0.01, 25, 9, 15, 10])
-		x.append([0, 1, 2, 3, 4])
-		sleep(2)
-		print(x)
+while True:
+	delta, day, month, hour, minute = 0.01, 25, 9, 15, 10
+
+	if delta >= 0.01: #постваить процент, после которого сделка совершится
+		bot.send_message(136563129, 'BitCoin\n----------\nΔ +%.2f$\nT %d.%d %d:%d' % (delta, day, month, hour, minute)) #-1001124440739 #бота перенести в отдельный файл
+		#запись в базу данных
+
+	sleep(5)
