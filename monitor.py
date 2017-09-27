3d27595ec32dabc84b2ecd856d96ee9b66c69d1c
Poloz Alexey <polozhev@mail.ru>
2017-09-28 02:55:14 +0300

    Re

--- monitor.py
+++ monitor.py
@@ -10,79 +10,95 @@ on = lambda text, words: any([word in text for word in words])
 alphabet = 'qwertyuiopasdfghjklzxcvbnmйцукенгшщзхъёфывапролджэячсмитьбю'
 clean = lambda cont: str(''.join([i if i in alphabet else ' ' for i in cont])).split()
 
+def get():
+	try:
+		with db:
+			return db.execute("SELECT * FROM lastmessage")
+	except:
+		return get()
+
+def update(id, chat):
+	try:
+		with db:
+			db.execute("UPDATE lastmessage SET message=(?) WHERE id=(?)", (id, chat))
+	except:
+		update(id, chat)
+
 def insert(cur, exc, buy, chat, id, time):
-	with db:
-		db.execute("INSERT INTO operations (act, currency, changer, buy, per, meschat, mesid, time1) VALUES (1, ?, ?, ?, 0.03, ?, ?, ?)", (cur, exc, buy, chat, id, time))
+	try:
+		with db:
+			db.execute("INSERT INTO operations (act, currency, changer, buy, per, meschat, mesid, time1) VALUES (1, ?, ?, ?, 0.03, ?, ?, ?)", (cur, exc, buy, chat, id, time))
+	except:
+		insert(cur, exc, buy, chat, id, time)
 
 def monitor():
 	while True:
 #Список сообщений
 		x = []
-		with db:
-			for i in db.execute("SELECT * FROM lastmessage"):
-				chat, id = i
-				text = ''
+		for i in get():
+			chat, id = i
+			text = ''
 
+			try:
+				text = bot.forward_message(meid, chat, id + 1).text
+			except:
 				try:
-					text = bot.forward_message(meid, chat, id + 1).text
+					text = bot.forward_message(meid, chat, id + 2).text
 				except:
-					try:
-						text = bot.forward_message(meid, chat, id + 2).text
-					except:
-						id = 0
-					else:
-						id += 2
+					id = 0
 				else:
-					id += 1
+					id += 2
+			else:
+				id += 1
 
-				if id:
-					if text: #изображения
-						x.append([chat, id, text.lower()])
-					db.execute("UPDATE lastmessage SET message=(?) WHERE id=(?)", (id, chat))
+			if id:
+				if text: #изображения
+					x.append([chat, id, text.lower()])
+				update(id, chat)
 				
-				sleep(1)
+			sleep(1)
 
-		print(x)
-		for i in x:
-			buy = 0
-			exc = 0
-			cur = -1
-			count = 1.0
-			time = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
+	print(x)
+	for i in x:
+		buy = 0
+		exc = 0
+		cur = -1
+		count = 1.0
+		time = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
 
 #Распознание сигнала
-			if on(i[2], vocabulary['buy']):
-				buy = 2
-			elif on(i[2], vocabulary['sell']):
-				buy = 1
-			else:
-				buy = 0
+		if on(i[2], vocabulary['buy']):
+			buy = 2
+		elif on(i[2], vocabulary['sell']):
+			buy = 1
+		else:
+			buy = 0
 
-			for j in range(len(exchanges)):
-				if exchanges[j][0].lower() in i[2]:
-					exc = j
+		for j in range(len(exchanges)):
+			if exchanges[j][0].lower() in i[2]:
+				exc = j
+				break
+
+		t = 0
+		for j in range(1, len(currencies)):
+			text = clean(i[2])
+			#print(text)
+			if currencies[j][1].lower() in text or currencies[j][0].lower() in text:
+				print(j, currencies[j])
+				if t == 0:
+					t = 1
+				elif t == 1:
+					t = 2
 					break
 
-			t = 0
-			for j in range(1, len(currencies)):
-				text = clean(i[2])
-				#print(text)
-				if currencies[j][1].lower() in text or currencies[j][0].lower() in text:
-					print(j, currencies[j])
-					if t == 0:
-						t = 1
-					elif t == 1:
-						t = 2
-						break
-
-					cur = j
-			if t == 2: continue #
+				cur = j
+		if t == 2: continue #
 			
-			#вставить биржу сюда? всё равно нет смысла работы этого алгоритма без биржи
-			if cur >= 1:
-				insert(cur, exc, buy, chat, id, time)
-			elif buy >= 1:
-				bot.send_message(sendid, 'Не распознано')
-				bot.forward_message(sendid, chat, id)
+		#вставить биржу сюда? всё равно нет смысла работы этого алгоритма без биржи
+		if cur >= 1:
+			insert(cur, exc, buy, chat, id, time)
+		elif buy >= 1:
+			bot.send_message(sendid, 'Не распознано')
+			bot.forward_message(sendid, chat, id)
 
-			sleep(5)
+		sleep(5)
--- trade.py
+++ trade.py
@@ -6,8 +6,25 @@ sendid = meid
 trader = YoBit()
 
 def get():
-	with db:
-		return db.execute("SELECT * FROM operations")
+	try:
+		with db:
+			return db.execute("SELECT * FROM operations")
+	except:
+		return get()
+
+def update1(count, price, time2, id):
+	try:
+		with db:
+			db.execute("UPDATE operations SET count=(?), price=(?), time2=(?) WHERE id=(?)", (count, price, time2, id))
+	except:
+		update1(count, price, time2, id)
+
+def update2(id):
+	try:
+		with db:
+			db.execute("UPDATE operations SET time2=0 WHERE id=(?)", (id,))
+	except:
+		update2(id)
 
 def trade():
 	while True:
@@ -18,8 +35,8 @@ def trade():
 				if name in res:
 					time = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
 					#Покупка / продажа + контроль ошибок
-					db.execute("UPDATE operations SET count=(?), price=(?), time2=(?) WHERE id=(?)", (0, res[name]['buy'], time, i[0]))
+					update1(0, res[name]['buy'], time, i[0])
 					bot.send_message(sendid, 'YoBit - %s: %.10f - %.10f' % (currencies[i[2]][1], res[name]['buy'], res[name]['sell']))
 				else:
-					db.execute("UPDATE operations SET time2=0 WHERE id=(?)", (i[0],))
+					update2(i[0])
 					bot.send_message(sendid, 'YoBit - %s: Валюта не найдена' % currencies[i[2]][1])
