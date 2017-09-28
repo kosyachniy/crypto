b03f6d9c05fa776bcab6b1083f9fff4b99e41eee
Poloz Alexey <polozhev@mail.ru>
2017-09-28 21:22:45 +0300

    Stylesed

--- trade.py
+++ trade.py
@@ -28,11 +28,24 @@ class YoBit():
 	def trade(self, cur, count, price, buy):
 		name = currencies[cur][1].lower() + '_btc'
 		buys = 'buy' if buy != 1 else 'sell'
-		bot.send_message(sendid, 'self.trader.trade(%s, %s, %.8f, %.8f)' % (name, buys, price, count)) #
+		bot.send_message(sendid, 'self.trader.trade(\'%s\', \'%s\', %.8f, %.8f)' % (name, buys, price, count)) #
 		if buy != 1:
-			bot.send_message(sendid, 'self.trader.trade(%s, \'sell\', %.8f, %.8f)' % (name, price * 1.1, count * 0.5)) #
-			bot.send_message(sendid, 'self.trader.trade(%s, \'sell\', %.8f, %.8f)' % (name, price * 1.15, count * 0.3)) #
-			bot.send_message(sendid, 'self.trader.trade(%s, \'sell\', %.8f, %.8f)' % (name, price * 1.2, count * 0.1)) #
-			bot.send_message(sendid, 'self.trader.trade(%s, \'sell\', %.8f, %.8f)' % (name, price * 1.25, count * 0.1)) #
-		#self.trader.trade(name, buys, price, count)
-		return 1 #успешно ли прошла операция на бирже + синхронизация в конце дня / по исполнению ордеров
+			bot.send_message(sendid, 'self.trader.trade(\'%s\', \'sell\', %.8f, %.8f)' % (name, price * 1.1, count * 0.5)) #
+			bot.send_message(sendid, 'self.trader.trade(\'%s\', \'sell\', %.8f, %.8f)' % (name, price * 1.15, count * 0.3)) #
+			bot.send_message(sendid, 'self.trader.trade(\'%s\', \'sell\', %.8f, %.8f)' % (name, price * 1.2, count * 0.1)) #
+			bot.send_message(sendid, 'self.trader.trade(\'%s\', \'sell\', %.8f, %.8f)' % (name, price * 1.25, count * 0.1)) #
+		'''
+		try:
+			self.trader.trade(name, buys, price, count)
+			if buy != 1:
+				self.trader.trade(name, 'sell', price * 1.1, count * 0.5)
+				self.trader.trade(name, 'sell', price * 1.15, count * 0.3)
+				self.trader.trade(name, 'sell', price * 1.2, count * 0.1)
+				self.trader.trade(name, 'sell', price * 1.25, count * 0.1)
+		except:
+			return 0
+		else:
+			return 1
+		'''
+		return 1
+		#синхронизация по исполнению ордеров
