30b84685a6b052b24d9951e5e9329a57199a0344
Poloz Alexey <polozhev@mail.ru>
2017-10-27 21:31:11 +0300

    Re

--- bot.py
+++ bot.py
@@ -1,54 +1,73 @@
 from func.main import *
+from pump import pump
 
 try:
 	num = messages.find_one({'$orderby': {'_id': -1}})['id']
 except:
 	num = 0
 
+messages = db['messages']
+exchangers = [i[0] for i in exchangers]
+
+#Меню
+from telebot import types
+
+def keyboard(chat, *cat):
+	x = types.ReplyKeyboardMarkup(resize_keyboard=True)
+	for j in cat:
+		x.add(*[types.KeyboardButton('/' + i) for i in j])
+	return bot.send_message(chat, 'Меню:', reply_markup=x)
+
+'''
+@bot.message_handler(commands=['start', 'help'])
+def start(message):
+	bot.send_message(message.chat.id, 'Привет! Я авто-трейд бот на биржах криптовалют!\n\nНапиши мне сигнал и я выставлю ордеры на биржах, буду контроллировать их на стоп-лосс и исполнение.')
+	if message.chat.id in admin:
+		bot.send_message(message.chat.id, 'Присоединённые биржи: ...')
+	else:
+		bot.send_message(message.chat.id, 'Для пользования ботом, его нужно активировать. Свяжитесь с нами здесь: @kosyachniy')
+'''
+
+@bot.message_handler(commands=['main', 'Назад'])
+def main(message):
+	if message.chat.id in admin:
+		keyboard(message.chat.id, exchangers, ['PUMP', 'Информация'])
+
+@bot.message_handler(commands=['PUMP', 'pump', 'памп', 'ПАМП', 'Памп'])
+def pumps(message):
+	if message.chat.id in admin:
+		#bot.send_message(message.chat.id, 'Памп на бирже: ')
+		x = keyboard(message.chat.id, exchangers, ['Назад'])
+		bot.register_next_step_handler(x, pumpss)
+
+def pumpss(message):
+	if message.chat.id in admin:
+		pump(message.text)
+
+@bot.message_handler(commands=['Информация', 'Инфо', 'info'])
+def info(message):
+	if message.chat.id in admin:
+		bot.send_message(message.chat.id, 'Информация с бирж')
+		#main(message)
+
+@bot.message_handler(commands=exchangers)
+def stockss(message):
+	if message.chat.id in admin:
+		keyboard(message.chat.id, ['Сигнал', 'Покупка', 'Продажа', 'ПАМП', 'Инфо', 'Назад'])
+
+#Дальнейшая обработка
 @bot.message_handler(content_types=["text"])
 def text(message):
-	print('!!!')
 	try:
 		chat, id, text = message.forward_from_chat.id, message.forward_from_message_id, message.text
 	except:
 		chat, id, text = message.chat.id, message.message_id, message.text
 
-	if chat in admin:
-		'''
-#Команда
-		if text in [*exchangers, 'PUMP', 'Информация']:
-			smart(chat, text)
-		'''
-#Дальнейшая обработка
+	if message.chat.id in admin:
 		num += 1
 		doc = {'id': num, 'chat': chat, 'message': id, 'text': text}
-		table.insert(doc)
+		messages.insert(doc)
 	else:
-		bot.send_message(chat, 'У вас нет доступа!')
+		bot.send_message(message.chat.id, 'У вас нет доступа!')
 
-'''
-from info import info
-from pump import pump
-
-#Управление
-def smart(chat, text):
-	if text == 'Информация':
-		info()
-	elif text == 'PUMP':
-		keyb = types.ReplyKeyboardMarkup(resize_keyboard=True)
-		keyb.add(types.KeyboardButton('Назад'))
-		bot.register_next_step_handler(bot.send_message(chat, 'Введите криптовалюту', reply_markup=keyb), pumpit)
-	else:
-		keyb = types.ReplyKeyboardMarkup(resize_keyboard=True)
-		keyb.add(types.KeyboardButton('Сигнал', 'Покупка', 'Продажа', 'Назад'))
-		bot.register_next_step_handler(bot.send_message(chat, exchangers.index(text), reply_markup=keyb), text) #не присоединяет биржу
-
-def pumpit(message):
-	if message.text == 'Назад':
-		bot.send_message(chat, reply_markup=keyboard())
-	else:
-		pump(message.text)
-'''
-
-if __name__ == '__main__':
-	bot.polling(none_stop=True)
+bot.polling(none_stop=True)
