10b1bd5773237560dfb06048a0da4e03726f26d4
Poloz Alexey <polozhev@mail.ru>
2017-11-20 11:48:41 +0300

    New structure of pump

--- test/pump/pump.py
+++ test/pump/pump.py
@@ -1,45 +1,48 @@
-fix_price = 0.0006
-delta = 1.4
-
-from time import sleep #, gmtime
+from time import sleep, gmtime
 import sys, re
+
+#from func.telegram import *
 from func.trade import stock
+
+#Telegram
+import telebot
+
+token = '417063852:AAFvfJdVGgLv9odlnY_gaiMmV4NIBMlgvOQ'
+#chat, id = -1001133674353, 883
+#meid = 136563129
+
+bot = telebot.TeleBot(token)
 
 texted = lambda x: re.sub('[^a-z]', '', x)
 
 def pump(chat, text, exc=0):
 	send = lambda x: bot.send_message(chat, x)
 
-	#запуск из терминала
 	exc = int(exc) #
 	text = texted(text)
-	#временное ограничение и продажа
-	#start = gmtime().tm_min
+	start = gmtime().tm_min
 
-	price = stock[exc].price(text) #
+	price = stock[exc].price(text)
 
 	try:
-		volume = fix_price / price #stock[exc].info() * 0.5
+		volume = 0.0006 / price #stock[exc].info() * 0.95
 	except:
-		send('Ошибка сумм!')
+		send('Ошибка!')
 		return 0
 
 	order = stock[exc].trade(text, volume, price)
 	send('%s\n%fɃ' % (order, price))
 
 	while not stock[exc].order(order):
-		sleep(1)
-		'''
 		if gmtime().tm_min - start >= 1:
 			stock[exc].cancel(order)
 			send('Ошибка покупки!')
 			return 0
-		'''
 
-	#sleep(1)
+	sleep(1)
 	send('Успешно куплено!')
 
-	price *= delta
+	price *= 1.4
 	order = stock[exc].trade(text, volume * 0.999999, price, 'sell')
 	send('%s\n%fɃ' % (order, price))
 
@@ -47,25 +50,10 @@ def pump(chat, text, exc=0):
 		if stock[exc].order(order):
 			send('Успешно продано!')
 			return 0
-		'''
 		else:
 			if gmtime().tm_min - start >= 60:
 				return 0
 			sleep(5)
-		'''
-
-#Telegram
-import telebot
-
-#заменить на основного бота
-token = '417063852:AAFvfJdVGgLv9odlnY_gaiMmV4NIBMlgvOQ'
-
-bot = telebot.TeleBot(token)
-
-#заменить на изображение
-@bot.message_handler(content_types=["text"])
-def text(message):
-	pump(message.chat.id, message.text)
 
 if __name__ == '__main__':
-	bot.polling(none_stop=True)
+	pump([i for i in sys.argv[1:]])
