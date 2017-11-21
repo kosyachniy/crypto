0e0583297b4c795646475ece59b783d83352eadf
Poloz Alexey <polozhev@mail.ru>
2017-11-22 00:43:49 +0300

    Re

--- autoadd.py
+++ autoadd.py
@@ -1,31 +1,55 @@
-from func import *
+from func.data import db
+from func.user import *
+
+messages = db['messages']
+try:
+	id = messages.find().sort('id', -1)[0]
+except:
+	id = 0
+
+from_id = -1001149594247 #-1001143136828
+
+get = lambda x=1: client.get_message_history(client.get_entity(from_id), x)[1]
 
 def autoadd():
+	channels = db['channels']
+	num = 0
+	for i in channels.find({'id': from_id}):
+		num = i['message']
+	if not num:
+		channels.save({'id': from_id, 'message': get()[0].id})
+
 	while True:
-		with db:
-			for i in db.execute("SELECT * FROM lastmessage"):
-				chat, id = i
-				text = ''
+		last = get()[0].id
+		for i in get(last - num):
+			if i.id > num:
+				#print(i)
 
 				try:
-					text = bot.forward_message(meid, chat, id + 1).text
+					x = i.message
 				except:
-					try:
-						text = bot.forward_message(meid, chat, id + 2).text
-					except:
-						id = 0
-					else:
-						id += 2
-				else:
-					id += 1
+					x = ' '
 
-				if id:
-					if text: #изображения
-						with open('data/messages.txt', 'a') as file:
-							print(json.dumps([chat, id, text.lower()], ensure_ascii=False), file=file)
-					db.execute("UPDATE lastmessage SET message=(?) WHERE id=(?)", (id, chat))
-				
-				sleep(5)
+				try:
+					x += i.media.caption
+				except:
+					pass
+
+				#+изображения
+
+				doc = {'id': id, 'chat': from_id, 'message': id, 'text': text}
+				messages.insert(doc)
+				'''
+				if text:
+					with open('data/messages.txt', 'a') as file:
+						print(json.dumps([chat, id, text.lower()], ensure_ascii=False), file=file)
+				db.execute("UPDATE lastmessage SET message=(?) WHERE id=(?)", (id, chat))
+				'''
+		
+		if last > num:
+			channels.save({})
+
+		sleep(5)
 
 if __name__ == '__main__':
 	autoadd()
--- groupread.py
+++ groupread.py
@@ -1,21 +1,10 @@
 from func.user import *
-from telethon.tl.types import PeerUser, PeerChat, PeerChannel
 
 from_id = -1001149594247 #-1001143136828
 to_id = 136563129 #440219158
 
-'''
-l = PeerChannel(channel_id=from_id).stringify().format()
-print(l)
-'''
-#print(dir(client))
-print(client.get_entity(from_id))
-
 for i in client.get_message_history(client.get_entity(from_id))[1]:
-	'''
-	print(i.id)
-	print(i)
-	'''
+	#print(i)
 
 	y = '%d %d\n' % (from_id, i.id)
 
@@ -29,10 +18,6 @@ for i in client.get_message_history(client.get_entity(from_id))[1]:
 	except:
 		pass
 
-	'''
-	if not len(x):
-		print('STOP')
-		break
-	'''
+	#+изображения
 
 	client.send_message(to_id, y + x + '\n--------------------')
