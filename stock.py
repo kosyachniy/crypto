155d3952b566280efba520631ba04de47be30764
Poloz Alexey <polozhev@mail.ru>
2017-11-22 18:03:01 +0300

    Bug infiniti sell after cancel order

--- stock.py
+++ stock.py
@@ -23,9 +23,14 @@ while True:
 			if not i['order'] and table.find_one({'message': i['message'], 'type': 'buy'})['success']:
 				i['order'] = stock[i['exchanger']].trade(i['currency'], i['count'], i['price'], 1)
 
-				rub = stock[i['exchanger']].ru()
-				formated = 'Продать %s!\n-----\nК %.8f\nɃ %.8f (%d₽)\n∑ %.8f (%d₽)' % (currencies[i['currency']][1], i['count'], i['price'], i['price'] / rub, i['price'] * i['count'], (i['price'] * i['count']) / rub)
-				send(formated)
+				if i['order']:
+					rub = stock[i['exchanger']].ru()
+					formated = 'Продать %s!\n-----\nК %.8f\nɃ %.8f (%d₽)\n∑ %.8f (%d₽)' % (currencies[i['currency']][1], i['count'], i['price'], i['price'] / rub, i['price'] * i['count'], (i['price'] * i['count']) / rub)
+					send(formated)
+				else:
+					send('Ошибка продажи!')
+					i['success'] = 2
+					table.save(i)
 
 #Если продажа исполнена
 			elif stock[i['exchanger']].order(i['order']):
