518d88c57dc9dd14e3c0ede1a8b2b51fcf85bf73
Poloz Alexey <polozhev@mail.ru>
2017-11-29 15:29:37 +0300

    Fixed volume

--- info.py
+++ info.py
@@ -3,7 +3,7 @@ from func.main import *
 #сделать регулярным по расписанию celery
 if __name__ == '__main__':
 	while True:
-		if gmtime().tm_hour == 20 + utc:
+		if gmtime().tm_hour - utc in (6, 12, 20):
 			for j in range(len(stock)):
 				send(stock[j].all())
 		sleep(3600)
