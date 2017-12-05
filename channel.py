156fdb0fe0f00e0f85c3847a179422e43072e153
Poloz Alexey <polozhev@mail.ru>
2017-12-06 00:49:04 +0300

    Fixed graph bug

--- channel.py
+++ channel.py
@@ -102,9 +102,8 @@ def channel():
 			if pric:
 				#print('stock[' + str(excd if i['exchanger'] == -1 else i['exchanger']) + '].last(' + str(i['currency']) + ', ' + str(i['out'][0][2] if i['out'][0][1] else pric * i['out'][0][2]) + ')')
 				stock[excd if i['exchanger'] == -1 else i['exchanger']].last(i['currency'], i['out'][0][2] if i['out'][0][1] else pric * i['out'][0][2])
-				f = open('re.png', 'rb')
-				bot.send_photo(channelid, f)
-				bot.send_photo(twochannel, f)
+				bot.send_photo(channelid, open('re.png', 'rb'))
+				bot.send_photo(twochannel, open('re.png', 'rb'))
 
 if __name__ == '__main__':
 	channel()
