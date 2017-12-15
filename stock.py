8c8e0530b608d51ce0074b83c95626e7830ca940
Poloz Alexey <polozhev@mail.ru>
2017-12-15 23:44:00 +0300

    Bugs

--- stock.py
+++ stock.py
@@ -99,6 +99,7 @@ while True:
 					i['order'] = stock[i['exchanger']].trade(i['currency'], i['count'], stock[i['exchanger']].price(i['currency'], 1), 1)
 					i['price'] = sell
 					send('Вышло время на продаже №%d' % (i['message'],))
+				'''
 #Все убыльные после моратория
 				elif sell < table.find_one({'message': i['message'], 'type': 'buy'})['price'] and now() - stamp(i['time']) >= timeloss:
 					send('После маротория ордер признан убыльным!')
@@ -114,6 +115,5 @@ while True:
 						for j in table.find({'message': i['message'], 'type': 'sell'}):
 							j['loss'] = i['loss']
 							table.save(j)
-				'''
 
 		table.save(i)
